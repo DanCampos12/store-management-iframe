1f88fc9056a4eeb9f2c335e1e128d4882e97c902
Danilo Rodrigues <dan43892@gmail.com>
2022-02-22 09:32:38 -0300

    adicao de comunicacao para enviar numero de usuarios

--- src/components/UserListComponent.ts
+++ src/components/UserListComponent.ts
@@ -1,9 +1,14 @@
 import { User } from '@/models'
 import { Component, Vue } from 'vue-property-decorator'
-import { Mutation, State } from 'vuex-class'
+import { Action, Mutation, State } from 'vuex-class'
 
 @Component
 export default class UserListComponent extends Vue {
+  @Action('getUsers') readonly getUsers!: () => Promise<User[]>
   @State('users') readonly users!: User[]
   @Mutation('removeUser') readonly removeUser!: (indexUser: number) => void
+
+  async created (): Promise<void> {
+    await this.getUsers()
+  }
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,44 +1,58 @@
 import { User } from '@/models'
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex, { Commit } from 'vuex'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    users: [
-      {
-        id: 0,
-        name: 'Danilo Campos',
-        email: 'danilo.rodrigues@meuportfol.io',
-        role: 'Júnior'
-      },
-      {
-        id: 1,
-        name: 'Leonardo Campos',
-        email: 'leonardo.campos@meuportfol.io',
-        role: 'Júnior'
-      },
-      {
-        id: 2,
-        name: 'Rafael Neves',
-        email: 'rafael.neves@meuportfol.io',
-        role: 'Tech Lead'
-      }
-    ] as User[],
+    users: [] as User[],
     broadcast: new BroadcastChannel('user-list')
   },
   mutations: {
+    setUsers (state, users: User[]) {
+      state.users = [...users]
+      state.broadcast.postMessage({
+        notification: 'user:counter',
+        value: state.users.length
+      })
+    },
     addUser (state, user: User) {
       state.users.push(user)
-      state.broadcast.postMessage('user:added')
+      state.broadcast.postMessage({ notification: 'user:added' })
     },
     removeUser (state, userIndex: number) {
       state.users.splice(userIndex, 1)
-      state.broadcast.postMessage('user:removed')
+      state.broadcast.postMessage({ notification: 'user:removed' })
     }
   },
   actions: {
+    getUsers ({ commit }: { commit: Commit }) {
+      const response = {
+        data: [
+          {
+            id: 0,
+            name: 'Danilo Campos',
+            email: 'danilo.rodrigues@meuportfol.io',
+            role: 'Júnior'
+          },
+          {
+            id: 1,
+            name: 'Leonardo Campos',
+            email: 'leonardo.campos@meuportfol.io',
+            role: 'Júnior'
+          },
+          {
+            id: 2,
+            name: 'Rafael Neves',
+            email: 'rafael.neves@meuportfol.io',
+            role: 'Tech Lead'
+          }
+        ] as User[]
+      }
+      commit('setUsers', response.data)
+      return response.data || []
+    }
   },
   modules: {
   }
